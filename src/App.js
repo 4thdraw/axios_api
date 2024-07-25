cc3fd520575a8fa1c4ebbc8dd4347e58bae3edd3
4thdraw <4thdraw2@gmail.com>
2024-07-25 11:52:23 +0900

    open api 적용하기

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useForm } from "react-hook-form"
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+  console.log(watch("example")) 
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <ul>  
+          <li> 
+            <label>성함</label>
+            <input  defaultValue="기본값" {...register("wr_name")} />
+          </li>
+          <li>
+            <label>아이디</label>
+            <input {...register("wr_id", { required: true })} />
+          </li>   
+          <li>
+            {errors.wr_id && <span>This field is required</span>}   
+            <input type="submit" value='회원가입' />
+          </li>     
+      </ul>
+    </form>
+  )
+}
